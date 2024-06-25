35fd743731057d4b5e991b3ff2dfef4f0aa191ab
spanandequipment <ilya.i@skinact.com>
2024-06-25 16:56:12 -0700

    checkout auth form

--- src/Controller/Customer/StripeSubscription.php
+++ src/Controller/Customer/StripeSubscription.php
@@ -22,6 +22,24 @@ class StripeSubscription extends \XLite\Controller\Customer\ACustomer
         return \XLite::getController()->getURL();
     }
 
+    public function isAuthorized()
+    {
+        return $this->getProfile();
+    }
+    public function displayScriptData()
+    {
+        $data = [
+            "url_params" => [
+                'target' => 'login',
+                'widget' => '\XLite\View\Authorization',
+                'fromURL' => \XLite::getController()->getURL(),
+                'popup' => '1',
+            ]
+        ];
+
+        echo ('<script type="text/x-cart-data">' . "\r\n" . json_encode($data) . "\r\n" . '</script>' . "\r\n");
+    }
+
     /**
      * @return bool
      */
